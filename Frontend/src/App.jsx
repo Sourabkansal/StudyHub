1dc42c799166b486bde8fe25f549960213ecb7e8
Sourabkansal <sourabkansal56@gmail.com>
2025-07-17 11:26:46 +0530

    add MIDDELWARE

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Outlet } from 'react-router-dom'
-import NavBar from './component/NavBar'
-import { Provider } from 'react-redux'
-import { store } from './Store/store'
+import NavBar from './component/NavBar'  
+import { Provider } from 'react-redux'   
+import { store } from './Store/store'    
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +15,6 @@ function App() {
      <NavBar/>
      <Outlet/>
    </Provider>
-
    </>
   )
 }
